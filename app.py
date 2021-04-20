cbba7557ca6be4f3121088db20680abeff9694c2
Shikha Rani <66298011+shikharani2805@users.noreply.github.com>
2021-04-20 18:20:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+
+import ipapi
+from flask import Flask, request, render_template
+import requests
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def Index():
+
+    search = request.form.get('search')
+    data = ipapi.location(ip=search, output='json')
+    return render_template('index.html', data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
